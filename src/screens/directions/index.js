a5fdcc711028c5d86e2a9427840e59987b1fef0e
Mohsin Ali <mohsinansari3011@gmail.com>
2018-10-31 16:15:56 +0500

    Get Directions and Show Calender

--- src/screens/directions/index.js
+++ src/screens/directions/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-//import swal from 'sweetalert';
+import swal from 'sweetalert';
 import * as firebase from '../../config/firebase'
 //import { withRouter, Link, Redirect, Route, browserHistory } from "react-router-dom";
 import { withScriptjs, withGoogleMap, InfoWindow, GoogleMap, Marker, GoogleApiWrapper } from "react-google-maps"
 //const providerx = firebase.provider;
-
+import Calendar from 'react-calendar';
 
 
 
@@ -16,6 +16,9 @@ class directionscreen extends Component {
         this.state = {
             currentuser: '',
             coords: null,
+            locationNear : [],
+            showcalender: false,
+            date: new Date(),
         };
 
 
@@ -44,12 +47,12 @@ class directionscreen extends Component {
             if (user) {
              
                 this.setState({ currentuser: user });
-                this.setPosition();
+                // this.setPosition();
                 console.log("componentDidMount");
 
-                setTimeout(() => {
-                    this.ExploreApiCoords();
-                }, 2000);
+                // setTimeout(() => {
+                //     this.ExploreApiCoords();
+                // }, 2000);
               
                
 
@@ -64,12 +67,28 @@ class directionscreen extends Component {
 
 
     }
-
+    // getDerivedStateFromProps(props ,state)
+    // {
+    //     console.log(props, "getDerivedStateFromProps", state);
+        
+    // }
 
     componentWillMount(){
+        //const { locationNear } = this.state;
 
-        //console.log("componentWillMount");
-       
+        console.log("componentWillMount");
+        this.setPosition();
+
+         setTimeout(() => {
+                this.ExploreApiCoords();
+                }, 1000);
+
+
+
+        
+
+        //this.setState({ locmarker: marker});
+
     }
     componentDidUpdate() {
         //console.log("componentDidUpdate");
@@ -86,10 +105,10 @@ class directionscreen extends Component {
     // }
 
     ExploreApiCoords() {
-        const { coords } = this.state;
-        console.log("ExploreApiCoords", coords)
+        const { coords, locmarkers } = this.state;
+        //console.log("ExploreApiCoords", coords)
         if (coords) {
-            let locationNear = [];
+            //let locationNear = [];
             var i = 1;
             let baseurl = "https://api.foursquare.com/v2/venues/explore?client_id=R115HVNIDCG01UMD2PMZAJMBM3EPYZJOCRJXW2RNEEUIZABL&client_secret=ITIEJIPPTZWZSEWTPAGYBQ24D344WZW3KRVKDZDB4NW3DJCN&v=20181030&ll=";
             let url = baseurl.concat(coords.latitude, ",", coords.longitude);
@@ -100,22 +119,61 @@ class directionscreen extends Component {
                  
                  locations.map(explore => {
 
-                      if (i < 4) {
-                            let lat = explore.venue.location.lat;
-                            let lng = explore.venue.location.lng;
-                          
+                      if (i < 5) {
+                            let Mlat = explore.venue.location.lat;
+                            let Mlng = explore.venue.location.lng;
+                           const index = i + 1;
 
-                          locationNear.push({ Coordinates: { latitude: lat, longitude: lng } } );
-                            //console.log(explore.venue.id, explore.venue.name, explore.venue.location.address
-                            //, explore.venue.location.lat, explore.venue.location.lng);
+
+                         
+                          var locmarker =  (<Marker
+                              key={index}
+                              title={explore.venue.name}
+                            position={{ lat: Mlat, lng: Mlng }}
+                            draggable={true}
+                          >  {
+                                  <InfoWindow>
+                                      <span>{explore.venue.name} <br /><button>Get Directions</button><button onClick={this.showCalenderscreen.bind(this)}>Next</button></span>
+                                  </InfoWindow>
+                            }
+                              </Marker>);
+
+                          i>1?
+                          this.setState({ 
+                                  
+                            locmarkers: [...this.state.locmarkers ,locmarker] 
+                        
+                              }) : this.setState({
+
+                                  locmarkers: locmarker
+
+                              })
+
+
+                        //   this.setState({
+                        //       locationNear: this.state.locationNear.concat(
+                        //           {
+                        //               nearlocations: {
+                        //                   coordinates: { latitude: Mlat, longitude: Mlng }, venue: explore.venue.name
+                        //               }
+                        //             }
+                        //       )
+                            
+                        //       }
+                        //     )
+                          //locationNear.push({ coordinates: { latitude: lat, longitude: lng } } );
+                          //console.log(explore.venue.id, explore.venue.name, explore.venue.location.address
+                          //, explore.venue.location.lat, explore.venue.location.lng);
+                         
                           i++;
-                      }
+                        }
                        
                     })
+                    
                 });
 
             //console.log(locationNear, " locationNear");
-            this.setState({ locations : locationNear});
+            
         }
 
 
@@ -123,22 +181,33 @@ class directionscreen extends Component {
 
     }
 
-
+    
 
     profileScreen4() {
-        const { coords, locations} = this.state;
-       
-        //console.log(coords," profileScreen4");
-        console.log(locations, " locationNearprofileScreen4");
+        const { coords, locmarkers} = this.state;
 
-        if (locations) {
-            locations.map(explore => {
-                console.log(explore, " item");
-            })
-            }
+
       
 
+
+        //console.log(coords," profileScreen4");
+        //console.log(locationNear, " location ",);
+       
+        // if (locations) {
+        //     locations.map(explore => {
+        //         console.log(explore, " item");
+        //     })
+        //     }
+      
+        
+            
+        
+
+
+        //console.log(locmarkers , " marker");
         return (<div>
+
+
 
             {coords && <MyMapComponent
                 isMarkerShown
@@ -147,7 +216,8 @@ class directionscreen extends Component {
                 containerElement={<div style={{ height: `80vh`, width: `100vh` }} />}
                 mapElement={<div style={{ height: `80%`, width: `100%` }} />}
                 coords={coords}
-                //coordslocations={locationNear}
+                locmarker={locmarkers}
+               
             />}
 
 
@@ -156,18 +226,69 @@ class directionscreen extends Component {
     }
 
 
+showCalenderscreen(){
+
+    //const { showcalender } = this.state;
+
+    this.setState({ showcalender : true});
+}
 
 
+onChange = date => this.setState({ date })
+
+onsendRequest()
+{
+
+    swal("info","Send the request?","info");
+
+    swal({
+        title: "Send the request?",
+        text: "Do you want to Send the request? !!!!",
+        icon: "info",
+        buttons: ["Cancel", "Yes"],
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                swal("Poof! Your Request has been Sent!", {
+                    icon: "success",
+                });
+
+                this.props.history.push("/dashboard");
+                //this.setState({ showmapdirections : true});
+
+            } else {
+                //swal("Your imaginary file is safe!");
+            }
+        });
+
+}
+
+
+  DateTimeSelectionScreen(){
+
+      return (<div><Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+      />
+          <div>  <button onClick={this.onsendRequest.bind(this)}>Send Request</button> </div>
+      </div>);
+
+
+    }
 
 
 
 
     render() {
 
-        //const { locationNear } = this.state;
-        //console.log("render", coords);
-        
-        return (<div> {this.profileScreen4() }</div>)
+        const { locmarkers, showcalender } = this.state;
+
+        return (<div> {
+            !showcalender ?
+            locmarkers && this.profileScreen4() :
+            this.DateTimeSelectionScreen()
+
+        }</div>)
     }
 
 
@@ -179,25 +300,14 @@ class directionscreen extends Component {
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
 
     
-    <GoogleMap 
-        defaultZoom={14}
+    props.locmarker ? <GoogleMap 
+        defaultZoom={12}
         center={{ lat: props.coords.latitude, lng: props.coords.longitude }}
     >
-        {/* {console.log(props.coordslocations, " prop1")}
-        {props.coordslocations ? props.coordslocations.map(data => {  
-            { console.log(data , " prop") }
-
-            
-    
-        }): <div>aasdasd</div> } */}
-
-       
-       
-
         
+        {props.locmarker}
 
-
-    </GoogleMap>
+    </GoogleMap> : <div> Loading Map...... </div>
     
 ))
 
